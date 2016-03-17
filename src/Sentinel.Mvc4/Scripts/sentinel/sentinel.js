6915688b9dd5bb9b88900aff3eaae9d7dbea9945
Gibran Silva <gibran.silva@gmail.com>
2016-03-17 18:12:27 -0300

    Inclusão dos pacotes nuget e controller e view
    Inclusão do controler e view

---
+++ src/Sentinel.Mvc4/Scripts/sentinel/sentinel.js
@@ -0,0 +1,33 @@
+﻿var TestResult = function (testData) {
+    var self = this;
+
+    self.name = testData.name;
+    self.description = testData.description;
+    self.state = testData.eventTypeName;
+    self.message = testData.message;
+};
+
+var TestViewModel = function () {
+    var self = this;
+
+    self.testResults = ko.observableArray();
+
+    self.Init = function () {
+        $.ajax({
+            dataType: 'json',
+            type: 'GET',
+            url: '/api/sentinel',
+            success: function (data) {
+                for (var i = 0; i < data.length; i++) {
+                    self.testResults.push(new TestResult(data[i]));
+                }
+            },
+            fail: function (error) {
+            }
+        });
+    }
+
+    this.Init();
+};
+
+ko.applyBindings(new TestViewModel());
---
+++ src/Sentinel.Mvc4/readme.txt
@@ -0,0 +1,29 @@
+﻿﻿Before you can run sentinel, you need to setup Sentinel.
+
+Add the following code in your SentinelConfig.cs:
+[assembly: OwinStartup(typeof(Startup))]
+namespace <NAMESPACE>
+{
+	public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseSentinel(new SentinelOptions
+            {
+                TestResultStore = new InMemoryTestResultStore(),
+                Tests = new List<SentinelTestBase>
+                {
+                    //Add tests class
+                },
+                OnTestResultChange = result =>
+                {
+					//Callback code to test runned
+                }
+            });
+        }
+    }
+}
+
+To configure tests for your website's sentinel, please see:
+
+	App_Start/Startup.cs
